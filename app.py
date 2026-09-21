dd6835fbaedb46841a741c775efb9370e4501fbd
Ivan <167475529+ivanm987@users.noreply.github.com>
2026-09-21 18:29:39 -0500

    Add asset paths for images and audio

---
+++ app.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+BASE = Path(__file__).parent
+ASSETS = BASE / "assets"
+
+orquidea = ASSETS / "orquidea.jpg"
+middlemist = ASSETS / "middlemist.jpg"
+cerezo = ASSETS / "cerezo.jpg"
+tulipan = ASSETS / "tulipan.jpg"
+gatitos = ASSETS / "gatitos.jpg"
+yellow = ASSETS / "yellow.mp3"
